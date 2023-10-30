9ba50d7fc2bcc1ce5612102eca6461ff7f467ca3
hellgrenj <hellgrenj@gmail.com>
2023-10-30 07:42:16 +0100

    new version

--- src/cli.rs
+++ src/cli.rs
@@ -19,7 +19,7 @@ pub fn parse_args() -> Settings {
             std::process::exit(0);
         }
         if arg == "--version" {
-            println!("{}", "v0.2.1".yellow().bold());
+            println!("{}", "v0.2.2".yellow().bold());
             std::process::exit(0);
         }
         if arg == "-h" {
